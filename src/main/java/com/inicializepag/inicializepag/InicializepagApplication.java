7198d763e147cbbe8e36f590683be3559266a5bf
SrCotocoMaster <andersonsutil.off@gmail.com>
2021-08-30 08:33:21 -0300

    Initial Commit

---
+++ src/main/java/com/inicializepag/inicializepag/InicializepagApplication.java
@@ -0,0 +1,13 @@
+package com.inicializepag.inicializepag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InicializepagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InicializepagApplication.class, args);
+	}
+
+}
