d71ec9753ef933d4e6d5c7d6f668b3f16b9fab08
João Marcos <sapomaggot07@gmail.com>
2021-08-31 09:30:19 -0300

    criando metado delete

--- src/main/java/com/inicializepag/inicializepag/controller/PagamentoController.java
+++ src/main/java/com/inicializepag/inicializepag/controller/PagamentoController.java
@@ -4,6 +4,7 @@ import com.inicializepag.inicializepag.model.Pagamentos;
 import com.inicializepag.inicializepag.repository.PagamentosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -40,6 +41,18 @@ public class PagamentoController {
                     return pagamentosRepository.save(pagamentos);
                 })
                 .orElseGet(() -> pagamentosRepository.save(pagamentosAtualizado));
+    }
+
+    @DeleteMapping("/delete/{Id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Long> delete(@PathVariable Long Id) {
+
+        if (Id != null) {
+            pagamentosRepository.deleteById(Id);
+            return ResponseEntity.ok(Id);
+        } else {
+            return (ResponseEntity<Long>) ResponseEntity.notFound();
+        }
     }
 }
 
